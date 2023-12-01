c1cb33712db34eeb9cc6997ed9b4ee01076929bf
Brian Haas <brianjohnhaas@gmail.com>
2023-12-01 09:20:45 -0500

    update

--- print.pl
+++ print.pl
@@ -1,27 +1,22 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-
-my @params = @ARGV;
 unless (@params = @ARGV) {
     die "usage: $0 [-t|-s] <number list | range>\n";
 }
 
-my $delimeter = pop @params;
+$delimeter = pop @params;
 
-if (defined($delimeter) && ($delimeter eq '-s' || $delimeter eq '-t')) {
+if ($delimeter eq '-s' || $delimeter eq '-t') {
     if ($delimeter eq '-s') {
-        $delimeter = " ";
+        $delimeter = '\s+';
     } elsif ($delimeter eq '-t') {
-        $delimeter = "\t";
+        $delimeter = '\t';
     }
     pop @ARGV;
 } else {
-    $delimeter = "\t";
+    $delimeter = '\t';
 }
 
-my @array;
 if ($ARGV[0] =~ /(^\d+)\-(\d+)/) {
     #print "$1\t$2\n";
     @array = ($1 .. $2);
@@ -29,25 +24,42 @@ if ($ARGV[0] =~ /(^\d+)\-(\d+)/) {
 } else {
     @array = @ARGV;
 }
-    
-my %here;
-foreach my $entry (@array) {
-    $here{$entry} = 1;
+
+
+
+
+my $EXCLUSION_MODE = 0;
+
+foreach $entry (@array) {
+        
+    if ($entry < 0) {
+        if (! $EXCLUSION_MODE) { print STDERR " -- running in exclusion mode.\n"; }
+        $EXCLUSION_MODE = 1;
+    }
+
+    $here{ abs($entry) } = 1;
+
 }
 
 
 while (<STDIN>) {
     chomp;
     #my $tab = 0;
-    my @columns = split (/$delimeter/, $_);
-
-    my @fields;
-    for (my $i = 0; $i <= $#columns; $i++) {
-        if ($here{$i}) {
-            push (@fields, "$columns[$i]");
-        }
+    @columns = split (/$delimeter/, $_);
+    my $output = "";
+    for ($i = 0; $i <= $#columns; $i++) {
+		#if ($tab) { print "\t";}
+		if ( 
+            ($here{$i} && ! $EXCLUSION_MODE) 
+            ||
+            ($EXCLUSION_MODE && ! $here{$i}) 
+            ) {
+			#print $columns[$i];
+			$output .= "$columns[$i]\t";
+			#$tab = 1;
+		}
     }
-    
-    print join($delimeter, @fields) . "\n";
+	$output =~ s/\s+$//; # remove trailing ws.
+    print "$output\n";
 }
 
